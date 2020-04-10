cee09fef5e729e5085342b6d2f850d8cef1f5e8c
Eugene Gordin <eugene@centerid.com>
2020-04-09 21:22:54 -0700

    refactor sho page

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -3,11 +3,15 @@ import { persistReducer } from 'redux-persist';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
 import storage from 'redux-persist/lib/storage';
 
 const rootReducer = combineReducers ({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer,
+    shop: shopReducer
 })
 
 const persistConfig = {
